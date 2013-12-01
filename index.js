7541a286a8a4d45dfdb7a94340945b7755b29fd7
Alsotang <alsotang@gmail.com>
2013-12-01 15:09:52 +0800

    Finish to #first

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Lodash = {
+  // Arrays
   compact: function (arr) {
     return arr.reduce(function (mem, item) {
       return item && mem.push(item) && mem || mem;
@@ -16,8 +17,83 @@ var Lodash = {
     return resultArr;
   },
   findIndex: function (arr, callback, thisArg) {
-    // to be continue
-  }
+    var index, newCallback;
+    if (typeof callback === 'function') {
+      newCallback = callback;
+    } else if (typeof callback === 'object') {
+      newCallback = function (obj) {
+        for (var k in callback) {
+          if (callback[k] !== obj[k]) {
+            return false;
+          }
+        }
+        return true;
+      };
+    } else if (typeof callback === 'string') {
+      newCallback = function (obj) {
+        return obj[callback];
+      };
+    }
+    for (var i = 0; i < arr.length; i++) {
+      var obj = arr[i];
+      if (newCallback(obj)) {
+        index = i;
+        break;
+      }
+    }
+    return index;
+  },
+  findLastIndex: function () {
+    var self = this;
+    arguments = [].slice.call(arguments);
+    arguments[0] = arguments[0].slice().reverse();
+    return self.findIndex.apply(self, arguments);
+  },
+  first: function (arr, callback, thisArg) {
+    var result, newCallback;
+    if (!callback) {
+      result = arr[0];
+    } else if (typeof callback === 'number') {
+      result = arr.slice(0, callback);
+    } else {
+      if (typeof callback === 'function') {
+        newCallback = callback;
+      } else if (typeof callback === 'string') {
+        var property = callback;
+        newCallback = function (obj) {
+          return obj[property];
+        };
+      } else if (typeof callback === 'object') {
+        newCallback = function (obj) {
+          for (var k in callback) {
+            if (callback[k] !== obj[k]) {
+              return false;
+            }
+          }
+          return true;
+        };
+      }
+      result = [];
+      for (var i = 0; i < arr.length; i++) {
+        var item = arr[i];
+        if (!newCallback(item)) {
+          break;
+        } else {
+          result.push(item);
+        }
+      }
+    }
+    return result;
+  },
+  // Collections
+  pluck: function (objArr, prop) {
+    return objArr.map(function (obj) {
+      return obj[prop];
+    });
+  },
+  where: function () {
+
+  },
 };
 
 module.exports = Lodash;
--- test/learn_lodash.test.js
+++ test/learn_lodash.test.js
@@ -6,31 +6,78 @@ describe("test/learn_lodash.test.js", function () {
     true.should.be.true;
   });
 
-  describe("#compact", function () {
-    it('should compact the Array', function () {
-      var arr = [1, 2, 3, 4, false, null, 0, "", undefined, NaN, 5, 6];
-      _.compact(arr).should.eql([1, 2, 3, 4, 5, 6]);
+  describe('Arrays', function () {
+    describe("#compact", function () {
+      it('should compact the Array', function () {
+        var arr = [1, 2, 3, 4, false, null, 0, "", undefined, NaN, 5, 6];
+        _.compact(arr).should.eql([1, 2, 3, 4, 5, 6]);
+      });
     });
-  });
 
-  describe('#difference', function () {
-    it('should be differenced', function () {
-      _.difference([1, 2, 3, 4, 5], [5, 2, 10]).should.eql([1, 3, 4]);
+    describe('#difference', function () {
+      it('should be differenced', function () {
+        _.difference([1, 2, 3, 4, 5], [5, 2, 10]).should.eql([1, 3, 4]);
+      });
     });
-  });
 
-  describe('#findIndex', function () {
-    it('should find the index', function () {
+    describe('#findIndex', function () {
+      it('should find the index', function () {
+        var characters = [
+          { 'name': 'barney',  'age': 36, 'blocked': false },
+          { 'name': 'fred',    'age': 40, 'blocked': true },
+          { 'name': 'pebbles', 'age': 1,  'blocked': false }
+        ];
+        _.findIndex(characters, function(chr) {
+          return chr.age < 20;
+        }).should.equal(2);
+        _.findIndex(characters, { 'age': 36 }).should.equal(0);
+        _.findIndex(characters, 'blocked').should.equal(1);
+      });
+    });
+
+    describe('#findLastIndex', function () {
+      it('should find the last index', function () {
+        var characters = [
+          { 'name': 'barney',  'age': 36, 'blocked': true },
+          { 'name': 'fred',    'age': 40, 'blocked': false },
+          { 'name': 'pebbles', 'age': 1,  'blocked': true }
+        ];
+        _.findLastIndex(characters, function(chr) {
+          return chr.age > 30;
+        }).should.equal(1);
+        _.findLastIndex(characters, { 'age': 36 }).should.equal(2);
+        _.findLastIndex(characters, 'blocked').should.equal(0);
+      });
+    });
+
+    it('#first', function () {
+      _.first([1, 2, 3])
+        .should.equal(1);
+
+      _.first([1, 2, 3], 2)
+        .should.eql([1, 2]);
+
+      _.first([1, 2, 3], function(num) {
+        return num < 3;
+      })
+        .should.eql([1, 2]);
+
       var characters = [
-        { 'name': 'barney',  'age': 36, 'blocked': false },
-        { 'name': 'fred',    'age': 40, 'blocked': true },
-        { 'name': 'pebbles', 'age': 1,  'blocked': false }
+        { 'name': 'barney',  'blocked': true,  'employer': 'slate' },
+        { 'name': 'fred',    'blocked': false, 'employer': 'slate' },
+        { 'name': 'pebbles', 'blocked': true,  'employer': 'na' }
       ];
-      _.findIndex(characters, function(chr) {
-        return chr.age < 20;
-      }).should.equal(2);
-      _.findIndex(characters, { 'age': 36 }).should.equal(0);
-      _.findIndex(characters, 'blocked').should.equal(1);
+
+      _.first(characters, 'blocked')
+        .should.eql([{ 'name': 'barney', 'blocked': true, 'employer': 'slate' }]);
+
+      _.pluck(_.first(characters, { 'employer': 'slate' }), 'name')
+        .should.eql(['barney', 'fred']);
     });
   });
+
+  describe('#Collections', function () {
+
+  });
+
 });
